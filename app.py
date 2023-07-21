29ef9457c9acfd6fcd16a20a04113804c13822ac
leigh966 <leigh966@hotmail.com>
2023-07-21 20:54:32 +0100

    Switched all " and '

--- app.py
+++ app.py
@@ -6,86 +6,86 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
+cors = CORS(app, resources={r'/*': {'origins': '*'}})
 
-@app.route("/user", methods=["POST"])
+@app.route('/user', methods=['POST'])
 def register():
-   username = request.form["username"]
-   password = request.form["password"]
-   group_id = request.form["group_id"]
+   username = request.form['username']
+   password = request.form['password']
+   group_id = request.form['group_id']
    try:
     cardCompilerDbOperations.register_contributer(username, password,
                                                    group_id)
    except KeyError:
-     return "Group not found", 404
+     return 'Group not found', 404
    except ValueError:
-      return "Username taken in this group", 409
-   return "Contributer Registered", 201
+      return 'Username taken in this group', 409
+   return 'Contributer Registered', 201
 
 
-@app.route("/group", methods=["POST"])
+@app.route('/group', methods=['POST'])
 def create_group():
-  return str(cardCompilerDbOperations.create_group(request.form["group_name"])), 201
+  return str(cardCompilerDbOperations.create_group(request.form['group_name'])), 201
 
 
-@app.route("/card", methods=["POST"])
+@app.route('/card', methods=['POST'])
 def add_card():
-    session_id = request.headers["session_id"]
-    user_id = ""
+    session_id = request.headers['session_id']
+    user_id = ''
     try:
         user_id = authorization.get_user_id(session_id)
     except IndexError:
-        return "Bad Session", 401
-    cardCompilerDbOperations.add_card(request.form["card_text"], user_id)
-    return "Card Added", 201
+        return 'Bad Session', 401
+    cardCompilerDbOperations.add_card(request.form['card_text'], user_id)
+    return 'Card Added', 201
 
-@app.route("/login", methods=["POST"])
+@app.route('/login', methods=['POST'])
 def login():
     #WARNING: session_ids do not currently expire!!!
     try:
-        return authorization.login(request.form["username"], request.form["password"], request.form["group_id"]), 201
+        return authorization.login(request.form['username'], request.form['password'], request.form['group_id']), 201
     except Exception as Error:
         print(Error)
-        return "Login Failed", 401
+        return 'Login Failed', 401
 
 
-@app.route("/cards", methods=["GET"])
+@app.route('/cards', methods=['GET'])
 def get_all_cards():
    # DEBUG ONLY
-   cards=dbOperations.select("*", "cards", "True")
+   cards=dbOperations.select('*', 'cards', 'True')
    return str(cards), 200
 
-@app.route("/my_cards", methods=["GET"])
+@app.route('/my_cards', methods=['GET'])
 def get_my_cards():
-    session_id = request.headers["session_id"]
-    user_id = ""
+    session_id = request.headers['session_id']
+    user_id = ''
     try:
       user_id = authorization.get_user_id(session_id)
     except:
-       return "Bad session", 401
+       return 'Bad session', 401
     return jsonify(cardCompilerDbOperations.get_my_cards(user_id))
 
 from openpyxl import Workbook
 from datetime import datetime
-@app.route("/report", methods=["GET"])
+@app.route('/report', methods=['GET'])
 def get_report():
-    session_id = request.headers["session_id"]
-    user_id = ""
+    session_id = request.headers['session_id']
+    user_id = ''
     try:
       user_id = authorization.get_user_id(session_id)
     except:
-       return "Bad session", 401
-    where = f'contributer_id="{user_id}"'
-    group_id = dbOperations.select("group_id", "contributers", where)[0][0]
-    join = dbOperations.get_inner_join_expression("cards", "contributers", "cards.contributer_id=contributers.contributer_id")
-    cards = dbOperations.select("card_text", join, f'group_id="{group_id}"')
-    print("cards: ", cards)
+       return 'Bad session', 401
+    where = f"contributer_id='{user_id}'"
+    group_id = dbOperations.select('group_id', 'contributers', where)[0][0]
+    join = dbOperations.get_inner_join_expression('cards', 'contributers', 'cards.contributer_id=contributers.contributer_id')
+    cards = dbOperations.select('card_text', join, f"group_id='{group_id}'")
+    print('cards: ', cards)
     wb = Workbook()
     sheet = wb.active
     for y in range(0,len(cards)):
-       sheet["A"+str(y+1)] = cards[y][0]
-    now = datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
-    path = f"./reports/{user_id}-{now}.xls"
+       sheet['A'+str(y+1)] = cards[y][0]
+    now = datetime.now().strftime('%d-%m-%Y-%H-%M-%S')
+    path = f'./reports/{user_id}-{now}.xls'
     wb.save(path)
-    return send_file(path, download_name='report.xls', as_attachment=False)
+    return send_file(path, download_name="report.xls", as_attachment=False)
 
--- cardCompilerDbOperations.py
+++ cardCompilerDbOperations.py
@@ -6,47 +6,47 @@ def create_group(group_name):
     # generate game_id
     group_id = uuid.uuid4()
     # create group with name [group_name]
-    dbOperations.create_record("groups", "group_id,group_name", f"'{str(group_id)}','{group_name}'")
+    dbOperations.create_record('groups', 'group_id,group_name', f"'{str(group_id)}','{group_name}'")
     # return game_id
     return group_id
 
 def name_taken_in_group(name, group_id):
-    where = f'contributer_name="{name}" AND group_id="{group_id}"'
-    search = dbOperations.select("*", "contributers", where)
+    where = f"contributer_name='{name}' AND group_id='{group_id}'"
+    search = dbOperations.select('*', 'contributers', where)
     return len(search) > 0
 
 def register_contributer(contributer_name, contributer_password, group_id):
     if name_taken_in_group(contributer_name, group_id):
-        raise ValueError("That username is taken in this group")
+        raise ValueError('That username is taken in this group')
 
     # if the group_id does not exist
         # raise a key error
-    where = f'group_id="{group_id}"'
-    if len(dbOperations.select("*", "groups", where))==0:
-        raise KeyError("This group does not exist")
+    where = f"group_id='{group_id}'"
+    if len(dbOperations.select('*', 'groups', where))==0:
+        raise KeyError('This group does not exist')
     # hash password
     hashed_password = authorization.hash_password(contributer_password)
     # register new contributer with name [contributer_name], 
     #   password_hash [hashed_password] and group_id [group_id]
-    fields = "contributer_name,password_hash,group_id"
-    values = f'"{contributer_name}","{str(hashed_password)}","{group_id}"'
-    dbOperations.create_record("contributers", fields, values)
+    fields = 'contributer_name,password_hash,group_id'
+    values = f"'{contributer_name}','{str(hashed_password)}','{group_id}'"
+    dbOperations.create_record('contributers', fields, values)
     
 
 def add_card(card_text, contributer_id):
     # if contributer does not exists
         # raise not found error
-    where = f'contributer_id="{contributer_id}"'
-    if len(dbOperations.select("*", "contributers", where))==0:
-        raise KeyError("This contributer does not exist")
+    where = f"contributer_id='{contributer_id}'"
+    if len(dbOperations.select('*', 'contributers', where))==0:
+        raise KeyError('This contributer does not exist')
     # add a new card with card_text [card_text] and contributer_id [contributer_id]
-    fields = "card_text,contributer_id"
-    values = f'"{card_text}","{contributer_id}"'
-    dbOperations.create_record("cards", fields, values)
+    fields = 'card_text,contributer_id'
+    values = f"'{card_text}','{contributer_id}'"
+    dbOperations.create_record('cards', fields, values)
 
 def get_my_cards(contributer_id):
     # assumes that the contributer_id is valid
-    where = f'contributer_id="{contributer_id}"'
-    cards = dbOperations.select("card_id,card_text", "cards", where)
-    return [{"card_id": record[0], "card_text": record[1]} for record in cards]
+    where = f"contributer_id='{contributer_id}'"
+    cards = dbOperations.select('card_id,card_text', 'cards', where)
+    return [{'card_id': record[0], 'card_text': record} for record in cards]
     
