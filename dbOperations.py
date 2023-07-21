1dd4b82e2f4dc3bd79b5ecb32803667c74160833
leigh966 <leigh966@hotmail.com>
2023-07-21 20:08:54 +0100

    made some necisary changes for heroku

--- dbOperations.py
+++ dbOperations.py
@@ -1,28 +1,47 @@
-import sqlite3
+import psycopg2
 import random
-from db_config import DATABASE_FILENAME
+from db_config import DATABASE_NAME, PASSWORD, USER, HOST
 MAX_INT = (2**63)-1
 
+def getConnection():
+    return psycopg2.connect(dbname=DATABASE_NAME, user=USER, host=HOST, password=PASSWORD)
+
+def execute(command):
+    con = getConnection()
+    cur = con.cursor()
+    cur.execute(command)
+    con.commit()
+    cur.close()
+    con.close()
+
+
+def execute_with_cursor(command, cursor):
+    cursor.execute(command)
 
 def create_record(table_name,field_name, field_value):
     command = f'INSERT INTO {table_name} ({field_name}) VALUES ({field_value})'
-    print(command)
-    with sqlite3.connect(DATABASE_FILENAME) as connection:
-        connection.execute(command)
-        connection.commit()
+    execute(command)
 
 def generate_unique_field(table_name, field_name):
     potential_id = -1
-    with sqlite3.connect(DATABASE_FILENAME) as connection:
-        while potential_id < 0 or len(connection.execute("SELECT * FROM "+table_name+" WHERE "+field_name+"='"
-                                                         + str(potential_id) + "';").fetchall()) > 0:
+    con = getConnection()
+    cur = con.cursor()
+
+    while potential_id < 0 or len(select("*", table_name, f"{field_name}={str(potential_id)}")) > 0:
             potential_id = random.randint(0, MAX_INT)
+    con.commit()
+    cur.close()
+    con.close()
     return potential_id
 
 def select(SELECT, FROM, WHERE):
-    with sqlite3.connect(DATABASE_FILENAME) as connection:
-        print(f'SELECT {SELECT} FROM {FROM} WHERE {WHERE};')
-        return connection.execute(f'SELECT {SELECT} FROM {FROM} WHERE {WHERE};').fetchall()
+    con = getConnection()
+    cur = con.cursor()
+    execute_with_cursor(f'SELECT {SELECT} FROM {FROM} WHERE {WHERE};', cur)
+    ret = cur.fetchall()
+    cur.close()
+    con.close()
+    return ret
 
 def get_inner_join_expression(table1, table2, on):
     return f"{table1} INNER JOIN {table2} ON {on}"
