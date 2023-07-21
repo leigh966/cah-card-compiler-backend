083ebb00dded82a88c2aa92fa472304b826dc648
leigh966 <leigh966@hotmail.com>
2023-07-21 20:12:51 +0100

    set up wsgi

--- app.py
+++ app.py
@@ -89,6 +89,3 @@ def get_report():
     wb.save(path)
     return send_file(path, download_name='report.xls', as_attachment=False)
 
-if __name__ == '__main__':
-   app.secret_key='secretivekeyagain'
-   app.run(host="127.0.0.1", port=8000, debug=True)
---
+++ wsgi.py
@@ -0,0 +1,5 @@
+from app import app
+
+if __name__ == '__main__':
+   app.secret_key='secretivekeyagain'
+   app.run(host="127.0.0.1", port=8000, debug=True)
