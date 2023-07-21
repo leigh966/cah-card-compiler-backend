52f18be955a7e74ff278e77248324a83ba0d834c
leigh966 <leigh966@hotmail.com>
2023-07-21 20:41:31 +0100

    converted schema to postgres format

--- schema.sql
+++ schema.sql
@@ -4,27 +4,27 @@ DROP TABLE IF EXISTS cards;
 DROP TABLE IF EXISTS sessions;
 
 CREATE TABLE groups (
-    group_id TEXT PRIMARY KEY,
+    group_id CHAR(36) PRIMARY KEY,
     group_name VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE contributers (
-    contributer_id INTEGER PRIMARY KEY,
+    contributer_id BIGINT PRIMARY KEY,
     password_hash VARCHAR(100) NOT NULL,
     contributer_name VARCHAR(20) NOT NULL,
-    group_id TEXT NOT NULL,
+    group_id CHAR(36) NOT NULL,
     FOREIGN KEY(group_id) REFERENCES groups(group_id)
 );
 
 CREATE TABLE cards (
-    card_id INTEGER PRIMARY KEY,
+    card_id BIGINT PRIMARY KEY,
     card_text VARCHAR(50) NOT NULL,
-    contributer_id INTEGER NOT NULL,
+    contributer_id BIGINT NOT NULL,
     FOREIGN KEY(contributer_id) REFERENCES contributers(contributer_id)
 );
 
 CREATE TABLE sessions (
-    session_id TEXT PRIMARY KEY,
-    contributer_id INTEGER NOT NULL,
+    session_id CHAR(36) PRIMARY KEY,
+    contributer_id BIGINT NOT NULL,
     FOREIGN KEY(contributer_id) REFERENCES contributers(contributer_id)
 );
