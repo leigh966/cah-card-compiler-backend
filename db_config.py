8b1e8ee30ba30f8c74ef25a093f99eaba4e53e6c
leigh966 <leigh966@hotmail.com>
2023-07-21 20:33:23 +0100

    updated db config for postgres

--- db_config.py
+++ db_config.py
@@ -1,2 +1,8 @@
-DATABASE_FILENAME = 'database.db'
+import os
+import urllib.parse as urlparse
+url = urlparse.urlparse(os.environ['DATABASE_URL'])
+DATABASE_NAME = url.path[1:]
+HOST = url.hostname
+USER = url.username
+PASSWORD = url.password
 INIT_SCHEMA_FILENAME = 'schema.sql'
